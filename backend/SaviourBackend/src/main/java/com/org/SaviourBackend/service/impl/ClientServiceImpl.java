326a7ba9454a775cfc8905947ff7dc58108a50e1
Sahil <parvezsayyad0045@gmail.com>
2024-08-06 19:36:06 +0530

    minor changes

--- backend/SaviourBackend/src/main/java/com/org/SaviourBackend/service/impl/ClientServiceImpl.java
+++ backend/SaviourBackend/src/main/java/com/org/SaviourBackend/service/impl/ClientServiceImpl.java
@@ -15,48 +15,51 @@ import com.org.SaviourBackend.repository.ClientRepository;
 import com.org.SaviourBackend.service.ClientService;
 
 @Service
-public class ClientServiceImpl implements ClientService{
+public class ClientServiceImpl implements ClientService {
 	@Autowired
 	private ClientRepository clientRepository;
+
 	@Override
 	public ClientDto createClient(ClientDto clientDto) {
-		
-		Client client=ClientMapper.mapToClient(clientDto);
+
+		Client client = ClientMapper.mapToClient(clientDto);
 		client.setClientAddedDate(LocalDateTime.now());
-        client.setClientUpdatedDate(LocalDateTime.now());
+		client.setClientUpdatedDate(LocalDateTime.now());
 		Client savedClient = clientRepository.save(client);
 		return ClientMapper.mapToClientDto(savedClient);
 	}
-	
+
 	public ClientDto getClientById(Long clientId) {
-	Client client = clientRepository.findById(clientId)
-		.orElseThrow(() -> 
-			new ClientNotFoundException("Client Does not Exist"+clientId));
+		Client client = clientRepository.findById(clientId)
+				.orElseThrow(() -> new ClientNotFoundException("Client Does not Exist" + clientId));
 		return ClientMapper.mapToClientDto(client);
 	}
 
 	@Override
 	public List<ClientDto> getAllClient() {
 		List<Client> clients = clientRepository.findAll();
-		return clients.stream().map((client) -> ClientMapper.mapToClientDto(client))
-				.collect(Collectors.toList());
+		return clients.stream().map((client) -> ClientMapper.mapToClientDto(client)).collect(Collectors.toList());
 	}
 
 	@Override
 	public ClientDto updateClient(Long clientId, ClientDto updateClient) {
 
-		Client client = clientRepository.findById(clientId).orElseThrow(() -> 
-			new ClientNotFoundException("Client Does not Exist"+clientId));
-		
+		Client client = clientRepository.findById(clientId)
+				.orElseThrow(() -> new ClientNotFoundException("Client Does not Exist" + clientId));
+
 		client.setClientName(updateClient.getClientName());
 		client.setClientEmail(updateClient.getClientEmail());
 		client.setClientPhone(updateClient.getClientPhone());
 		client.setClientAddress(updateClient.getClientAddress());
 		client.setClientCompany(updateClient.getClientCompany());
 		client.setClientIndustry(updateClient.getClientIndustry());
-		client.setStatus(updateClient.getStatus());
+		if (updateClient.getStatus() == null || updateClient.getStatus().isEmpty()) {
+			client.setStatus("active"); // Default status
+		} else {
+			client.setStatus(updateClient.getStatus());
+		}
 		client.setClientUpdatedDate(LocalDateTime.now());
-		
+
 		Client updatedClientObj = clientRepository.save(client);
 		return ClientMapper.mapToClientDto(updatedClientObj);
 	}
@@ -64,11 +67,11 @@ public class ClientServiceImpl implements ClientService{
 	@Override
 	public void deleteClient(Long clientId) {
 
-		Client client = clientRepository.findById(clientId).orElseThrow(() -> 
-		new ClientNotFoundException("Client Does not Exist with given ID"+clientId));
-		
+		Client client = clientRepository.findById(clientId)
+				.orElseThrow(() -> new ClientNotFoundException("Client Does not Exist with given ID" + clientId));
+
 		clientRepository.deleteById(clientId);
-		
+
 	}
 
 }
