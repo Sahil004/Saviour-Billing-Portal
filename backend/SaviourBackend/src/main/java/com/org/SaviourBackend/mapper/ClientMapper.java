14f3c869f03aa95bbc17867581e9f7b37a6c5be3
Sahil <parvezsayyad0045@gmail.com>
2024-08-06 18:29:39 +0530

    updated client mappper class

--- backend/SaviourBackend/src/main/java/com/org/SaviourBackend/mapper/ClientMapper.java
+++ backend/SaviourBackend/src/main/java/com/org/SaviourBackend/mapper/ClientMapper.java
@@ -7,17 +7,31 @@ public class ClientMapper {
 	
 	public static ClientDto mapToClientDto(Client client) {
 		return new ClientDto(
-				client.getId(),
-				client.getName(),
-				client.getEmail()
+				client.getClinetId(),
+				client.getClientName(),
+				client.getClientEmail(),
+				client.getClientPhone(),
+				client.getClientAddress(),
+				client.getClientCompany(),
+				client.getClientIndustry(),
+				client.getClientAddedDate(),
+				client.getClientUpdatedDate(),
+				client.getStatus()
 				);		
 	}
 	
 	public static Client mapToClient(ClientDto clientDto) {
 		return new Client(
-				clientDto.getId(),
-				clientDto.getName(),
-				clientDto.getEmail()
+				clientDto.getClientId(),
+				clientDto.getClientName(),
+				clientDto.getClientEmail(),
+				clientDto.getClientPhone(),
+				clientDto.getClientAddress(),
+				clientDto.getClientCompany(),
+				clientDto.getClientIndustry(),
+				clientDto.getClientAddedDate(),
+				clientDto.getClientUpdatedDate(),
+				clientDto.getStatus()
 				);
 	}
 }
