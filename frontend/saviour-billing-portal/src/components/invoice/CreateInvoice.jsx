f52f6b3b6a4fc0dfe995902085db2583972ed58b
Sahil <parvezsayyad0045@gmail.com>
2024-08-07 23:12:25 +0530

    initiated invoice page

--- frontend/saviour-billing-portal/src/components/invoice/CreateInvoice.jsx
+++ frontend/saviour-billing-portal/src/components/invoice/CreateInvoice.jsx
@@ -1,78 +1,109 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { listClient } from '../../services/ClientService';
 
 const CreateInvoice = () => {
+
+    const [clients, setClients] = useState([]);
+    const [selectedClient, setSelectedClient] = useState('');
+    const [selectedClientId, setSelectedClientId] = useState('');
+
+    useEffect(() => {
+        listClient().then(response => {
+            setClients(response.data);
+        }).catch(error => console.error(error));
+    }, [])
+
+    const handleClientChange = (e) => {
+        const clientId = e.target.value;
+        setSelectedClientId(clientId);
+        const client = clients.find(c => c.clientId === parseInt(clientId));
+        setSelectedClient(client)
+    };
+
     return (
-        <div class="create-invoice bg-white rounded">
+        <div className="create-invoice bg-white rounded">
             <div className="page-header">
                 <h5 className=' p-4 mb-0'>Create Invoice</h5>
             </div>
             <hr className='m-0' />
             <div className="page-body p-4">
                 <form className='dashboard-form'>
-                    <div class="row form-section">
-                        <div class="form-group col-md-6 col-lg-4 pb-3">
-                            <label for="invoiceNumber" className='text-grey'>Invoice Number *</label>
-                            <input type="text" class="form-control rounded" id="invoiceNumber" placeholder="#58963" disabled />
+                    <div className="row form-section">
+                        <div className="form-group col-md-6 col-lg-4 pb-3">
+                            <label htmlFor="invoiceNumber" className='text-grey'>Invoice Number *</label>
+                            <input type="text" className="form-control rounded" id="invoiceNumber" placeholder="#58963" disabled />
                         </div>
-                        {/* <div class="form-group col-md-6 col-lg-4 d-flex align-items-end pb-3">
-                            <div class="w-100">
-                                <label for="selectClient">Select Client</label>
-                                <div class="input-group">
-                                    <select id="selectClient" class="form-control">
-                                        <option selected>Choose...</option>
-
-                                    </select>
-                                    <div class="input-group-append">
-                                        <button class="btn btn-outline-secondary" type="button">+ New Client</button>
-                                    </div>
-                                </div>
+                        <div className="form-group col-md-6 col-lg-4 pb-3">
+                            <label htmlFor="selectClient">Select Client</label>
+                            <div className="input-group">
+                                <select
+                                    name='selectClient'
+                                    id="selectClient"
+                                    className="form-select"
+                                    value={selectedClientId}
+                                    onChange={handleClientChange}>
+                                    <option value=''>Select Client</option>
+                                    {clients.map(client => (
+                                        <option key={client.clientId} value={client.clientId}>{client.clientName}</option>
+                                    ))
+                                    }
+                                </select>
+                                <button className="btn btn-success" type="button">+ New Client</button>
                             </div>
-                        </div> */}
-                    </div>
-                    <hr />
-                    <div class="row form-section">
-                        <div class="form-group col-md-4 pb-3">
-                            <label for="clientName">Client Name *</label>
-                            <input type="text" class="form-control rounded" id="clientName" placeholder="Alex Z." />
-                        </div>
-                        <div class="form-group col-md-4 pb-3">
-                            <label for="clientEmail">Client Email *</label>
-                            <input type="email" class="form-control rounded" id="clientEmail" placeholder="alex@company.com" />
-                        </div>
-                        <div class="form-group col-md-4 pb-3">
-                            <label for="clientContactNumber">Client Contact Number *</label>
-                            <input type="text" class="form-control rounded" id="clientContactNumber" placeholder="+00 00000 00000" />
-                        </div>
-                    </div>
-
-                    <div class="row form-section">
-                        <div class="form-group col-md-12 pb-3">
-                            <label for="clientAddress">Client Address *</label>
-                            <input type="text" class="form-control" id="clientAddress" placeholder="Enter Address" />
                         </div>
                     </div>
                     <hr />
-                    <div class="row form-section">
-                        <div class="form-group col-md-4 pb-3">
-                            <label for="invoiceDate">Invoice Date *</label>
-                            <div class="input-group">
-                                <input type="date" class="form-control" id="invoiceDate"  />
+                    <div className="row form-section">
+                        <div className="form-group col-md-6 col-lg-4 pb-3">
+                            <label htmlFor="clientName">Client Name *</label>
+                            <input
+                                type='text'
+                                id='clientName'
+                                name='clientName'
+                                value={selectedClient.clientName}
+                                placeholder='e.g., James Walter'
+                                className='form-control'
+                                disabled
+                            />
+                        </div>
+                        <div className="form-group col-md-4 pb-3">
+                            <label htmlFor="clientEmail">Client Email *</label>
+                            <input type="email" className="form-control rounded" id="clientEmail" placeholder="alex@company.com" />
+                        </div>
+                        <div className="form-group col-md-4 pb-3">
+                            <label htmlFor="clientContactNumber">Client Contact Number *</label>
+                            <input type="text" className="form-control rounded" id="clientContactNumber" placeholder="+00 00000 00000" />
+                        </div>
+                    </div>
+
+                    <div className="row form-section">
+                        <div className="form-group col-md-12 pb-3">
+                            <label htmlFor="clientAddress">Client Address *</label>
+                            <input type="text" className="form-control" id="clientAddress" placeholder="Enter Address" />
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="row form-section">
+                        <div className="form-group col-md-4 pb-3">
+                            <label htmlFor="invoiceDate">Invoice Date *</label>
+                            <div className="input-group">
+                                <input type="date" className="form-control" id="invoiceDate" />
                             </div>
                         </div>
-                        <div class="form-group col-md-4 pb-3">
-                            <label for="invoiceDate">Due Date *</label>
-                            <div class="input-group">
-                                <input type="date" class="form-control" id="dueDate"  />
+                        <div className="form-group col-md-4 pb-3">
+                            <label htmlFor="invoiceDate">Due Date *</label>
+                            <div className="input-group">
+                                <input type="date" className="form-control" id="dueDate" />
                             </div>
                         </div>
-                        <div class="form-group col-md-4 pb-3">
-                            <label for="status">Status *</label>
-                            <select id="status" class="form-control">
+                        <div className="form-group col-md-4 pb-3">
+                            <label htmlFor="status">Status *</label>
+                            <select id="status" className="form-control">
                                 <option selected>Pending</option>
                             </select>
                         </div>
                     </div>
-                    <hr/>
+                    <hr />
                     <div className='dashboard-form-btn text-end'>
                         <button type="submit" className='btn btn-primary px-4 py-2 fw-semibold'>Save</button>
                     </div>
